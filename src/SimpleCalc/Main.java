7f663aee37fdba9c112514a641d45ea85f0c5ae1
Xue64 <rufelleemmanuelpactol7@gmail.com>
2022-12-15 01:18:56 +0800

    Add SimpleCalc main function

---
+++ src/SimpleCalc/Main.java
@@ -0,0 +1,14 @@
+package SimpleCalc;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleCalcGUI gui = new SimpleCalcGUI();
+        gui.setContentPane(gui.getPanel1());
+        gui.setVisible(true);
+        gui.setSize(600, 250);
+        gui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
