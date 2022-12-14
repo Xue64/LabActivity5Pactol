ea3236cc2139c27ccbb977852d74cffa36381c1d
Xue64 <rufelleemmanuelpactol7@gmail.com>
2022-12-15 01:14:21 +0800

    Rename SimpleCalc components

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,10 +1,18 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class SimpleCalcGUI {
+public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
-    private JTextField textField1;
-    private JComboBox comboBox1;
-    private JButton computeResultButton;
+    private JTextField tfNumber1;
+    private JComboBox cbOperations;
+    private JButton btnCompute;
+    private JTextField tfNumber2;
+    private JTextField lblResult;
+
+    public SimpleCalcGUI() throws HeadlessException {
+        this.setTitle("Simple Calculator");
+    }
 }
+
